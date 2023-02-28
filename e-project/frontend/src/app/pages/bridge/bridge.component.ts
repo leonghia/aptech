c9ebfafd2628ad416da4a988801245832a99b4a7
leonghia <leonghiacnn@gmail.com>
2023-03-01 00:44:53 +0700

    created lightbox

--- e-project/frontend/src/app/pages/bridge/bridge.component.ts
+++ e-project/frontend/src/app/pages/bridge/bridge.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { fadeIn } from 'src/app/shared/animations';
 import { ApiService } from 'src/app/shared/api.service';
@@ -10,12 +10,15 @@ import { ApiService } from 'src/app/shared/api.service';
   styleUrls: ['./bridge.component.css']
 })
 export class BridgeComponent implements OnInit {
+  @ViewChild('modal') modal!: ElementRef;
+  @ViewChild('modalImg') modalImg!: ElementRef;
   selectedTab: string = 'tab-info';
   bridgeID!: string;
   bridge!: any;
   bridgeImages!: any[];
   selectedImage!: any;
   enterViewport: boolean = false;
+  modalShown: boolean = false;
 
   constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
@@ -32,17 +35,28 @@ export class BridgeComponent implements OnInit {
     });
   }
 
-  onVisibilityChange(status: boolean) {
+  onVisibilityChange(status: boolean): void {
     if (status)
       this.enterViewport = status;
   }
 
-  onClickImage(image: any) {
+  onClickImage(image: any): void {
     this.selectedImage = image;
   }
 
-  onClickBtnTab(tab: string) {
+  onClickBtnTab(tab: string): void {
     this.selectedTab = tab;
+  }
+
+  onShowModal(modalSrc: string): void {
+    this.modalShown = true;
+    this.modal.nativeElement.classList.remove('hidden');
+    this.modalImg.nativeElement.src = modalSrc;
+  }
+
+  onCloseModal(): void {
+    this.modalShown = false;
+    this.modal.nativeElement.classList.add('hidden');
   }
   
 }
