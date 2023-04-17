0f78bcfa369426e871575cd5c97715c267c3babe
leonghia <leonghiacnn@gmail.com>
2023-04-17 10:32:41 +0700

    update Exam

--- jp1/exam/Exam/src/dev/nghia/Main.java
+++ jp1/exam/Exam/src/dev/nghia/Main.java
@@ -9,22 +9,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         Account account = new Account();
-        try {
-            account.input();
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-        }
-
+        account.input();
         System.out.print("Deposit (0) or Withdraw (1): ");
         int type = Integer.parseInt(scanner.nextLine());
         System.out.print("Enter amount of money ($): ");
         long amount = Integer.parseInt(scanner.nextLine());
-        try {
-            account.depositAndWithdraw(amount, type);
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-        }
-
+        account.depositAndWithdraw(amount, type);
         System.out.println(account);
     }
 }
