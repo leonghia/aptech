f06ce43f5a5c851bcb2dbef9c932e0d989700c8e
leonghia <leonghiacnn@gmail.com>
2023-05-30 16:34:03 +0700

    submit UsersJSON

---
+++ mlj/exercises/UsersJSON/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+namespace UsersJSON
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string url = "https://dummyjson.com/users";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string responBody = await response.Content.ReadAsStringAsync();
+
+                    // Deserialize the JSON response
+                    UsersData? usersData = JsonSerializer.Deserialize<UsersData>(responBody);
+
+                    // Access and process the JSON data as needed
+                    usersData.Display();
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+
+            }
+        }
+    }
+}
---
+++ mlj/exercises/UsersJSON/UsersData.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersJSON
+{
+    internal class UsersData
+    {
+
+        public User[] users { get; set; }
+        public int total { get; set; }
+        public int skip { get; set; }
+        public int limit { get; set; }
+
+        internal class User
+        {
+            public int id { get; set; }
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string maidenName { get; set; }
+            public int age { get; set; }
+            public string gender { get; set; }
+            public string email { get; set; }
+            public string phone { get; set; }
+            public string username { get; set; }
+            public string password { get; set; }
+            public string birthDate { get; set; }
+            public string image { get; set; }
+            public string bloodGroup { get; set; }
+            public decimal height { get; set; }
+            public decimal weight { get; set; }
+            public string eyeColor { get; set; }
+            public Hair hair { get; set; }
+            public string domain { get; set; }
+            public string ip { get; set; }
+            public Address address { get; set; }
+            public string macAddress { get; set; }
+            public string university { get; set; }
+            public Bank bank { get; set; }
+            public Company company { get; set; }
+            public string ein { get; set; }
+            public string ssn { get; set; }
+            public string userAgent { get; set; }
+
+            public override string ToString()
+            {
+                return $"id: {id}\n" +
+                    $"firstName: {firstName}\n" +
+                    $"lastName: {lastName}\n" +
+                    $"maidenName: {maidenName}\n" +
+                    $"age: {age}\n" +
+                    $"gender: {gender}\n" +
+                    $"email: {email}\n" +
+                    $"phone: {phone}\n" +
+                    $"username: {username}\n" +
+                    $"password: {password}\n" +
+                    $"birthDate: {birthDate}\n" +
+                    $"image: {image}\n" +
+                    $"bloodGroup: {bloodGroup}\n" +
+                    $"height: {height}\n" +
+                    $"weight: {weight}\n" +
+                    $"eyeColor: {eyeColor}\n" +
+                    $"hair: \n" +
+                    $"{hair}\n" +
+                    $"domain: {domain}\n" +
+                    $"ip: {ip}\n" +
+                    $"address: \n" +
+                    $"{address.ToString(1)}\n" +
+                    $"macAddress: {macAddress}\n" +
+                    $"university: {university}\n" +
+                    $"bank: \n" +
+                    $"{bank}\n" +
+                    $"company: \n" +
+                    $"{company}\n" +
+                    $"ein: {ein}\n" +
+                    $"ssn: {ssn}\n" +
+                    $"userAgent: {userAgent}\n";
+            }
+
+            internal class Hair
+            {
+                public string color { get; set; }
+                public string type { get; set; }
+
+                public override string ToString()
+                {
+                    return $"\tcolor: {color}\n" +
+                        $"\ttype: {type}\n";
+                }
+            }
+
+            internal class Address
+            {
+                public string address { get; set; }
+                public string city { get; set; }
+                public Coordinates coordinates { get; set; }
+                public string postalCode { get; set; }
+                public string state { get; set; }
+
+                public string ToString(int tabs)
+                {
+                    if (tabs == 1)
+                    {
+                        return $"\taddress: {address}\n" +
+                        $"\tcity: {city}\n" +
+                        $"\tcoordinates: \n" +
+                        $"{coordinates.ToString(2)}\n" +
+                        $"\tpostalCode: {postalCode}\n" +
+                        $"\tstate: {state}\n";
+                    }
+                    else
+                    {
+                        return $"\t\taddress: {address}\n" +
+                        $"\t\tcity: {city}\n" +
+                        $"\t\tcoordinates: \n" +
+                        $"{coordinates.ToString(3)}\n" +
+                        $"\t\tpostalCode: {postalCode}\n" +
+                        $"\t\tstate: {state}\n";
+                    }
+                    
+                }
+
+                internal class Coordinates
+                {
+                    public decimal lat { get; set; }
+                    public decimal lng { get; set; }
+
+                    public string ToString(int tabs)
+                    {
+                        switch (tabs)
+                        {
+                            case 1:
+                                return $"\tlat: {lat}\n" +
+                            $"\tlng: {lng}\n";
+                            case 2:
+                                return $"\t\tlat: {lat}\n" +
+                            $"\t\tlng: {lng}\n";
+                            case 3:
+                                return $"\t\t\tlat: {lat}\n" +
+                            $"\t\t\tlng: {lng}\n";
+                            default:
+                                return $"lat: {lat}\n" +
+                            $"lng: {lng}\n";
+                        }
+                        
+                    }
+                }
+            }
+
+            internal class Bank
+            {
+                public string cardExpire { get; set; }
+                public string cardNumber { get; set; }
+                public string cardType { get; set; }
+                public string currency { get; set; }
+                public string iban { get; set; }
+
+                public override string ToString()
+                {
+                    return $"\tcardExpire: {cardExpire}\n" +
+                        $"\tcardNumber: {cardNumber}\n" +
+                        $"\tcardType: {cardType}\n" +
+                        $"\tcurrency: {currency}\n" +
+                        $"\tiban: {iban}\n";
+                }
+            }
+
+            internal class Company
+            {
+                public Address address { get; set; }
+                public string department { get; set; }
+                public string name { get; set; }
+                public string title { get; set; }
+
+                public override string ToString()
+                {
+                    return $"\taddress: \n" +
+                        $"{address.ToString(2)}\n" +
+                        $"\tdepartment: {department}\n" +
+                        $"\tname: {name}\n" +
+                        $"\ttitle: {title}\n";
+                }
+            }
+        }
+
+        public void Display()
+        {
+            foreach (User user in users)
+            {
+                Console.WriteLine(user);
+                Console.WriteLine(new String('-', 150));
+            }
+        }
+    }
+
+}
