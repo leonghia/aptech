c2b70f04a8f4df27dce50c31d2d151ecd388acb7
Nghia La <leonghiacnn@gmail.com>
2024-01-24 10:16:52 +0700

    Run migrations

---
+++ DMAWS/exam/backend/EcomShop/OrderAPI/Migrations/20240124031607_CreateDbAndTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDbAndTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderTbl",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemCode = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    ItemName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    ItemQty = table.Column<int>(type: "int", nullable: false),
+                    OrderDelivery = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OrderAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderTbl", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderTbl");
+        }
+    }
+}
